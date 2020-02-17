c9886f8ee9ccb4aedae00dbd86bcdb940e2d4b89
Patrick Tran <patrick.tran06@gmail.com>
2020-02-17 12:32:37 -0800

    complete one vs all

--- machine-learning-ex3/ex3/oneVsAll.m
+++ machine-learning-ex3/ex3/oneVsAll.m
@@ -12,9 +12,10 @@ m = size(X, 1);
 n = size(X, 2);
 
 % You need to return the following variables correctly 
+% note: n + 1 accounts for the bias factor
 all_theta = zeros(num_labels, n + 1);
 
-% Add ones to the X data matrix
+% Add ones to the X data matrix (bias factor)
 X = [ones(m, 1) X];
 
 % ====================== YOUR CODE HERE ======================
@@ -49,12 +50,18 @@ X = [ones(m, 1) X];
 %                 initial_theta, options);
 %
 
+% for loop
+%  for a
 
+for c = 1:num_labels
+	initial_theta = zeros(n+1, 1);
+	options = optimset('GradObj', 'on', 'MaxIter', 50);
 
-
-
-
-
+    [theta] = ...
+        fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
+                initial_theta, options);
+    [all_theta(c,:)] = theta';
+end
 
 
 
