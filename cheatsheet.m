ba5f8097cbe61294c1444e39d82ae95c131addc0
Patrick Tran <patrick.tran06@gmail.com>
2020-02-27 11:40:41 -0800

    add cheatsheet

---
+++ cheatsheet.m
@@ -0,0 +1,13 @@
+% How to: print to terminal
+
+% How to: for loop; from range x to y (inclusive)
+for index = 1:10
+end
+
+% How to: for loop; over a column vector
+for index = 1:size(vector, 1)
+end
+
+% How to: for loop; over a row vector
+for index = 1:size(vector, 2)
+end
